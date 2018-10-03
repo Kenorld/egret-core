e406e328a4a1ea8556eb4ab3867b66b935a5abc9
Chrislearn Young <chris@acroidea.com>
2018-10-03 12:19:47 +0800

    error json format

--- panic.go
+++ panic.go
@@ -2,7 +2,6 @@ package egret
 
 import (
 	"fmt"
-	"net/http"
 	"runtime/debug"
 
 	"github.com/sirupsen/logrus"
@@ -25,15 +24,12 @@ func handleInvocationPanic(ctx *Context, err interface{}) {
 	nerr := NewErrorFromPanic(err)
 	// Only show the sensitive information in the debug stack trace in development mode, not production
 	if DevMode {
-		fmt.Println(err)
 		fmt.Println(string(debug.Stack()))
-		ctx.Response.Writer.WriteHeader(http.StatusInternalServerError)
-		ctx.Response.Writer.Write(debug.Stack())
 	} else {
 		logrus.WithFields(logrus.Fields{
 			"error": nerr,
 			"stack": string(debug.Stack()),
 		}).Error("error")
-		ctx.Error = nerr
 	}
+	ctx.Error = nerr
 }
